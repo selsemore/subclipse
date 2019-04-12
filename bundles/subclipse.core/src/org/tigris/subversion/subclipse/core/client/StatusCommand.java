9d28c26c12e8c6c3239b76d68bfcea4d0f04a13a
Stephen Elsemore <selsemore@collab.net>
2019-04-12 19:34:40 +0200

    scenario4 PS3 Commit 9

--- bundles/subclipse.core/src/org/tigris/subversion/subclipse/core/client/StatusCommand.java
+++ bundles/subclipse.core/src/org/tigris/subversion/subclipse/core/client/StatusCommand.java
@@ -46,7 +46,7 @@ public class StatusCommand {
     this.contactServer = contactServer;
   }
 
-  // 8
+  // 9
   protected void execute(final ISVNClientAdapter client, final IProgressMonitor monitor)
       throws SVNClientException {
     ISVNNotifyListener revisionListener =
